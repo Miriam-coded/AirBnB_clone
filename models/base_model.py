f4305ea7ec6c14a2d94e95a7f79f7b80e86653b8
Dennis Miringu <dennismiringu03@gmail.com>
2024-05-15 07:18:03 -0700

    base model and test for base model I will do 4 later

---
+++ models/base_model.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+contains BaseModel class
+"""
+
+from datetime import datetime
+import uuid
+
+
+class BaseModel:
+    """Super-class from which future classes will be derived"""
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
+
+    def to_dict(self):
+        inst_dict = self.__dict__.copy()
+        inst_dict["__class__"] = self.__class.__name__
+        inst_dict["created_at"] = self.created_at.isoformat()
+        inst_dict["updated_at"] = self.updated_at.isoformat()
+        return inst_dict
+
+    def save(self):
+        self.updated_at = datetime.utcnow()
+
+
+if __name__ == '__main__':
+    my_model = BaseModel()
+    my_model.name = "My First Model"
+    my_model.my_number = 89
+    print(my_model)
+    my_model.save()
+    print(my_model)
+    my_model_json = my_model.to_dict()
+    print(my_model_json)
+    print("JSON of my_model:")
+    for key in my_model_json.keys():
+        print("\t{}: ({}) - {}".format(key,
+              type(my_model_json[key]), my_model_json[key]))
