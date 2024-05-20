af8ab0da86b03cc62459acd5b742b0bd6e4360f5
Dennis Miringu <dennismiringu03@gmail.com>
2024-05-20 01:29:42 -0700

    try fixes

--- models/base_model.py
+++ models/base_model.py
@@ -51,7 +51,6 @@ class BaseModel:
         """Update updated_at with current datetime"""
         self.updated_at = datetime.utcnow()
         self._import_storage().save()
-        print("OK")
 
     def _import_storage(self):
         """Import storage method"""
