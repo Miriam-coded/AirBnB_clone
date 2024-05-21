05c7f6bc68086d5bb331e96a4444820005e70d3c
Miriam-coded <miriamwmukuhi@gmail.com>
2024-05-21 20:42:01 +0300

    Update BaseModel Unittest

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -6,6 +6,7 @@ Unittest classes:
     TestBaseModel_save
     TestBaseModel_to_dict
 """
+import uuid
 import os
 from datetime import datetime
 from time import sleep
@@ -16,34 +17,39 @@ from models.base_model import BaseModel
 class TestBaseModel(unittest.TestCase):
     """Test the BaseModel class"""
 
+    def setUp(self):
+        """Set up test methods"""
+        self.model = BaseModel()
+
     def test_inst(self):
-        my_model = BaseModel()
-        self.assertIsNotNone(my_model.id)
-        self.assertIsNotNone(my_model.created_at)
-        self.assertIsNotNone(my_model.updated_at)
+        """Test instance creation and atribute types"""
+        self.assertIsNotNone(self.model.id)
+        self.assertIsInstance(self.model.id, str)
+        self.assertIsNotNone(self.model.created_at)
+        self.assertIsInstance(self.model.created_at, datetime)
+        self.assertIsNotNone(self.model.updated_at)
+        self.assertIsInstance(self.model.updated_at, datetime)
 
     def test_to_dict(self):
-        my_model = BaseModel()
-        my_model_dict = my_model.to_dict()
+        my_model_dict = self.model.to_dict()
         self.assertIsInstance(my_model_dict, dict)
         self.assertEqual(my_model_dict["__class__"], 'BaseModel')
-        self.assertEqual(my_model_dict["id"], my_model.id)
+        self.assertEqual(my_model_dict["id"], self.model.id)
         self.assertEqual(my_model_dict["created_at"],
-                         my_model.created_at.isoformat())
+                         self.model.created_at.isoformat())
         self.assertEqual(my_model_dict["updated_at"],
-                         my_model.created_at.isoformat())
+                         self.model.updated_at.isoformat())
 
     def test_str(self):
-        my_model = BaseModel()
-        self.assertTrue(str(my_model).startswith('[BaseModel]'))
-        self.assertIn(my_model.id, str(my_model))
-        self.assertIn(str(my_model.__dict__), str(my_model))
+        self.assertTrue(str(self.model).startswith('[BaseModel]'))
+        self.assertIn(self.model.id, str(self.model))
+        self.assertIn(str(self.model.__dict__), str(self.model))
 
     def test_save(self):
-        my_model = BaseModel()
-        initial_updated_at = my_model.updated_at
-        current_updated_at = my_model.save()
-        self.assertNotEqual(initial_updated_at, current_updated_at)
+        initial_updated_at = self.model.updated_at
+        sleep(1)
+        self.model.save()
+        self.assertNotEqual(initial_updated_at, self.model.updated_at)
 
 
 if __name__ == "__main__":
